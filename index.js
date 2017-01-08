54d20ec87c5b3c2b0239ba4e0c19788b06dd1e70
li3p <li3p@qq.com>
2017-01-08 19:22:09 +0800

    migrate to webpack 2.x loader options

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 // themes loader makes theme variables available to each module during compilation process
 // its relative to the module/components sufolder of the vue-component folder
 // configuration within webpack.config.js stating theme locations is required for this to work
+var loaderUtils = require('loader-utils')
+
 module.exports = function(source) {
 	this.cacheable();
-	var config = this.options.themeLoader || false;
-	if (config && config.themes.length > 0) {
+	var query = loaderUtils.parseQuery(this.query);
+
+	var config = query.themes || false;
+	if (config && query.themes.length > 0) {
 		var result = "";
-		config.themes.forEach(function(theme){
+        query.themes.forEach(function(theme){
 			result += "@import '" + theme + "';\n";
 		});
 		return result += source;
